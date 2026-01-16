2e8fd787c70d917d83b348111a6a3d0bd8e283d4
Serap BALCI <81075168+reset-34@users.noreply.github.com>
2026-01-16 19:49:15 +0300

    Add project files.

---
+++ Hesap Makinası Programı/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HesapMakinesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = 0;
+            double num2 = 0;
+            string operation = "";
+
+            Console.WriteLine("Basit Hesap Makinesi");
+            Console.WriteLine("--------------------");
+
+            Console.Write("İlk sayıyı giriniz: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("İkinci sayıyı giriniz: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Yapılacak işlemi (+, -, *, /) giriniz: ");
+            operation = Console.ReadLine();
+
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine($"Sonuç: {num1 + num2}");
+                    break;
+                case "-":
+                    Console.WriteLine($"Sonuç: {num1 - num2}");
+                    break;
+                case "*":
+                    Console.WriteLine($"Sonuç: {num1 * num2}");
+                    break;
+                case "/":
+                    if (num2 != 0)
+                    {
+                        Console.WriteLine($"Sonuç: {num1 / num2}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sıfıra bölme hatası!");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Geçersiz işlem.");
+                    break;
+            }
+
+            Console.WriteLine("Hesaplama tamamlandı.");
+            Console.ReadKey();
+        }
+    }
+}
+
